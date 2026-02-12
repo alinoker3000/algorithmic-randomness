a1209e8efdd0f348f6d2ba42cf3c4f4531b1e290
alinoker3000 <alinoker3000@yandex.ru>
2026-02-12 20:22:59 +0300

    refactored for better testing

--- l1.py
+++ l1.py
@@ -1,12 +1,7 @@
-"""
-Первый уровень батареи L1 проверяет частотные (равновероятность 0 и 1)
-и серийные (количество переходов) закономерности.
-Это позволяет отсеивать простейшие генераторы и делает батарею эффективной
-на малых объемах данных.
-"""
-
 import math
 from collections import Counter
+
+import zlib
 
 
 def l1(data: str):
@@ -37,8 +32,31 @@ def l1(data: str):
     return p_value, t_stat, entropy, p_value >= alpha
 
 
+def l2(data: str):
+    alpha = 0.05
+    n_bits = len(data)
+
+    # zlib работает с байтами
+    byte_data = data.encode('utf-8')
+
+    compress_code_len = len(zlib.compress(byte_data, level=9)) * 8
+    empty_code_len = len(zlib.compress(b"", level=9)) * 8
+
+    code_len = max(0, compress_code_len - empty_code_len)
+
+    t_stat = n_bits - code_len
+    p_value = 2 ** (-max(0.0, t_stat))
+    compression = code_len / n_bits
+
+    return p_value, t_stat, compression, p_value >= alpha
+
+
 test_data1 = "10" * 10  # цикличная строка на 2 символа
 test_data2 = "0" * 10 + "1" * 10  # длинные серии
 
-print(l1(test_data1))
-print(l1(test_data2))
+test_data3 = "1101001011100001" * 1000
+
+
+print(l1(test_data1), l2(test_data1))
+print(l1(test_data2), l2(test_data2))
+print(l1(test_data3), l2(test_data3))
